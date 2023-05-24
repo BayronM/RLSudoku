711ff876c9a5effd51aba763048026aaaac40d89
Bayron Monsalvez <bayronmonsalvez@gmail.com>
2023-05-24 19:28:11 -0400

    Add enviroment

---
+++ enviroment/enviroment.py
@@ -0,0 +1,58 @@
+class Sudoku:
+    def __init__(self, board):
+        self.board = board
+
+    def print_board(self):
+        for i in range(9):
+            if i % 3 == 0 and i != 0:
+                print("- - - - - - - - - - - -")
+            for j in range(9):
+                if j % 3 == 0 and j != 0:
+                    print(" | ", end="")
+                if j == 8:
+                    print(self.board[i][j])
+                else:
+                    print(str(self.board[i][j]) + " ", end="")
+
+    def find_empty(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                if self.board[i][j] == 0:
+                    return (i, j)
+        return None
+
+    def valid(self, num, pos):
+        # Check row
+        for i in range(len(self.board[0])):
+            if self.board[pos[0]][i] == num and pos[1] != i:
+                return False
+        # Check column
+        for i in range(len(self.board)):
+            if self.board[i][pos[1]] == num and pos[0] != i:
+                return False
+        # Check box
+        box_x = pos[1] // 3
+        box_y = pos[0] // 3
+        for i in range(box_y*3, box_y*3 + 3):
+            for j in range(box_x * 3, box_x*3 + 3):
+                if self.board[i][j] == num and (i,j) != pos:
+                    return False
+        return True
+
+# Test this environment
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+sudoku = Sudoku(board)
+sudoku.print_board()
+print(sudoku.find_empty())
+print(sudoku.valid(5, (0, 2)))
